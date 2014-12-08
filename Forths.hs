5c9fdc31ece572a916e5bfea75d8d7f94b413729
Christopher Armstrong <radix@twistedmatrix.com>
2014-12-08 11:26:30 -0600

    some comments

--- Forths.hs
+++ Forths.hs
@@ -36,6 +36,12 @@ pretty (Free (FString s cont)) = "push " ++ s ++ ";\n" ++ pretty cont
 pretty (Free (FWord s cont)) = "call " ++ s ++ ";\n" ++ pretty cont
 pretty (Free End) = "end.\n"
 
+
+-- Confused about exactly how to do evalutaion.
+-- First, think about whether I want a side-effecting interpreter: not at first.
+-- I think I'd like a pure evaluation function of Forth -> Stack -> Stack,
+-- though it probably won't be useful for a real side-effecting interpreter.
+
 --main = do
 --     putStrLn $ show $ program
 
@@ -44,6 +50,7 @@ todo:
 
 - parse the following into the example program:
     3 2 add 100 add
+- interpreter
 - function definition
 - is there a way to define built-ins without having to be concerned about the
   tail of the stack?
